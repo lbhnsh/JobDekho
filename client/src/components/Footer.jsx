745b20d8ac73d4f32a1795f75de8cd6c42191e9a
labhansh naik <labhanshvnaik2104@gmail.com>
2024-05-15 18:26:07 +0530

    a

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,21 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="text-white mt-20">
-      <div className="overflow-x-hidden -mb-0.5">
-        <svg
-          preserveAspectRatio="none"
-          viewBox="0 0 1200 120"
-          xmlns="http://www.w3.org/2000/svg"
-          style={{
-            fill: "#1B7593",
-            width: "125%",
-            height: 112,
-            transform: "rotate(180deg)",
-          }}
-        >
-        </svg>
-      </div>
-
+      
       <div style={{ backgroundColor: '#1B7593' }}>
       <div style={{ maxWidth: '1200px', padding: '5rem 1.25rem', margin: '0 auto' }}>
         <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap', gap: '2.5rem', marginBottom: '-2.5rem' }}>
